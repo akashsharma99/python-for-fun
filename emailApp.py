4cf35db8540ca0152b903319c31ab4c6c37c3c5b
akashsharma99 <akash.asharma.sharma11@gmail.com>
2018-06-23 17:04:48 +0530

    Add files via upload

---
+++ emailApp.py
@@ -0,0 +1,21 @@
+from smtplib import SMTP, SMTPAuthenticationError
+import getpass
+host="smtp.gmail.com"
+port=587 #will be different for different email services
+username=input("Enter your mail id:\n")
+password=getpass.getpass()
+email_conn=SMTP(host, port)
+email_conn.ehlo()#to view these messages write inside print statement
+email_conn.starttls()#to view these messages write inside print statement
+try:
+    email_conn.login(username, password)
+    to_list=list(input("Enter recipients mail id seperated by a comma:\n").split(","))
+    message_text=input("Enter your message:\n")
+    email_conn.sendmail(username,to_list,message_text)
+except SMTPAuthenticationError:
+    print("Could not log in. Please check username and password.")
+except:
+    print("Some error occured mail not sent. Sorry!")
+
+
+email_conn.quit()#to logout
